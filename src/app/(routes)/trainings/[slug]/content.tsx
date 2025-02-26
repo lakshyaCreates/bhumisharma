1680c3b7395f90899b1faf7f6b309d76f24cc3e5
lakshyaCreates <lakshya.creates07@gmail.com>
2025-02-26 12:36:38 +0530

    feat: add training: `shield-up`

--- src/app/(routes)/trainings/[slug]/content.tsx
+++ src/app/(routes)/trainings/[slug]/content.tsx
@@ -4,7 +4,7 @@ import { MDX } from "@/components/mdx";
 
 export const Content = ({ content }: Training) => {
     return (
-        <div className="prose size-full dark:prose-invert">
+        <div className="prose size-full text-justify dark:prose-invert">
             <MDX content={content.mdx} />
             <div className="h-6"></div>
         </div>
--- src/components/mdx/index.tsx
+++ src/components/mdx/index.tsx
@@ -2,7 +2,10 @@
 
 import { MDXContent } from "@content-collections/mdx/react";
 
+import NextImage from "next/image";
 import Link from "next/link";
+
+import { cn } from "@/lib/utils";
 
 import { LinkPreview } from "@/components/aceternity/link-preview";
 import { Button } from "@/components/ui/button";
@@ -13,7 +16,30 @@ export const MDX = ({ content }: { content: string }) => {
     return (
         <MDXContent
             code={content}
-            components={{ LinkPreview, Link, Button, Highlight }}
+            components={{ LinkPreview, Link, Button, Highlight, Image }}
+        />
+    );
+};
+
+const Image = ({
+    src,
+    alt,
+    className,
+}: {
+    src: string;
+    alt: string;
+    className?: string;
+}) => {
+    return (
+        <NextImage
+            alt={alt}
+            src={src}
+            width={500}
+            height={500}
+            className={cn(
+                "aspect-auto w-full rounded-xl object-cover object-center",
+                className,
+            )}
         />
     );
 };
